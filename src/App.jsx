6384983b2423874adb361295f6b707fed11c8f46
Jeda@Win <salimbjed21@gmail.com>
2026-06-10 23:41:01 +0800

    manage product details

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import AuthProvider from "./context/AuthContext";
+import ProductDatails from "./pages/ProductDatails";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/products/:id" element={<ProductDatails />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </div>
