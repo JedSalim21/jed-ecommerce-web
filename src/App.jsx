48ed15f2df395ab963fa49ce75c38f5d14b11fa3
Jeda@Win <salimbjed21@gmail.com>
2026-06-10 22:45:21 +0800

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
+import AuthProvider from "./context/AuthContext";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 };
 
